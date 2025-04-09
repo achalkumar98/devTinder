85309208b408d69f2c171b07432d4b5fac4a16bb
achalkumar98 <hackerachal1620@gmail.com>
2025-04-10 01:02:14 +0530

    req print in console

--- src/app.js
+++ src/app.js
@@ -4,25 +4,19 @@ const app = express();
 
 const port = 7777;
 
-//Routing parameters
 
-// This will work for "/abc" and "/ac" also
-// app.get(/ab?c/, (req, res) => {
+// get method to get data from the server
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
 //   res.send({firstName: "Achal", lastName: "Kumar"}  );
-// });
+// }); 
 
 
-// "/abc", "/abbc", "/abbbc", "/abbbbbbbbbbbbbc" will work
-// "/ac", "/ab" will not work
 
-// app.get(/ab+c/, (req, res) => {
+// app.get("/user/:userId", (req, res) => {
+//   console.log(req.params);
 //   res.send({firstName: "Achal", lastName: "Kumar"}  );
-// });
-
-// it will support - "abcbcbcbcd"
-// app.get(/ab*cd/, (req, res) => {
-//   res.send({firstName: "Achal", lastName: "Kumar"}  );
-// });
+// }); 
 
 app.listen(port, () => {
   console.log("Server is running on port 7777");
