945e072f27d8a7736236c70e4dfb199237fbc968
achalkumar98 <hackerachal1620@gmail.com>
2025-04-10 00:49:55 +0530

    all the regix

--- src/app.js
+++ src/app.js
@@ -4,31 +4,25 @@ const app = express();
 
 const port = 7777;
 
-// Tthe will handle only Get request method
-app.get("/user", (req, res) => {
-  res.send({firstName: "Achal", lastName: "Kumar"}  );
-});
+//Routing parameters
 
-app.post("/user", (req, res) => {
-  res.send("Data Successfully Send to the database");
-})
+// This will work for "/abc" and "/ac" also
+// app.get(/ab?c/, (req, res) => {
+//   res.send({firstName: "Achal", lastName: "Kumar"}  );
+// });
 
-app.put("/user", (req, res) => {
-  res.send("Data Successfully updated to the database");
-});
 
-app.patch("/user", (req, res) => {
-  res.send("Data Successfully Minor updated to the database");
-});
+// "/abc", "/abbc", "/abbbc", "/abbbbbbbbbbbbbc" will work
+// "/ac", "/ab" will not work
 
-app.delete("/user", (req, res) => {
-  res.send("Data Successfully deleted from the database");
-});
+// app.get(/ab+c/, (req, res) => {
+//   res.send({firstName: "Achal", lastName: "Kumar"}  );
+// });
 
-// This will Handel all the http request methods like get, post, put, delete, patch
-app.use("/test", (req, res) => {
-  res.send("Test route");
-});
+// it will support - "abcbcbcbcd"
+// app.get(/ab*cd/, (req, res) => {
+//   res.send({firstName: "Achal", lastName: "Kumar"}  );
+// });
 
 app.listen(port, () => {
   console.log("Server is running on port 7777");
