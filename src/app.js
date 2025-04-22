b67e1e97e0bcedb8aa9250f127a950f66225a136
achalkumar98 <hackerachal1620@gmail.com>
2025-04-22 13:38:31 +0530

    install mongoose and creating a modle and also creating dummy new user

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
-
 const port = 7777;
 
+app.post("/signup", async (req, res) => {
+  // Creating a instance of the user model
+  const user = new User({
+    firstName: "Achal",
+    lastName: "Kumar",
+    email: "achal1620@gmail.com",
+    password: "12345678",
+  });
 
-// get method to get data from the server
-// app.get("/user", (req, res) => {
-//   console.log(req.query);
-//   res.send({firstName: "Achal", lastName: "Kumar"}  );
-// }); 
-
-
-
-// app.get("/user/:userId", (req, res) => {
-//   console.log(req.params);
-//   res.send({firstName: "Achal", lastName: "Kumar"}  );
-// }); 
-
-app.listen(port, () => {
-  console.log("Server is running on port 7777");
+  try {
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error in adding user: " + err.message);
+  }
 });
+
+connectDB()
+  .then(() => {
+    console.log("Database connection Established...");
+    app.listen(port, () => {
+      console.log("Server is running on port", port);
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected", +err);
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://hackerachal1620:Achal1620@learningnodejs-cluster1.jmy1lyk.mongodb.net/devTinder"
+  );
+};
+
+module.exports = connectDB;
